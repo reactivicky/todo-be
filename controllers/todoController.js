4d94904272b4612ba2923d77edf8619fca49e3b6
Vignesh <reactivicky@gmail.com>
2024-02-12 21:00:48 +0530

    created deleteTodo functionality

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -5,7 +5,7 @@ const CustomError = require("../utils/customError");
 
 const getAllTodos = tryCatch(async (req, res) => {
   const todos = await client.query(
-    "SELECT * FROM todos ORDER BY updated_at DESC"
+    "SELECT * FROM todos ORDER BY updated_at DESC;"
   );
   res.status(200).json({
     status: "sucess",
@@ -57,7 +57,7 @@ const getTodo = tryCatch(async (req, res) => {
   if (!success) {
     throw new CustomError("Id should be a number and is required", 403);
   }
-  const query = `SELECT * FROM todos WHERE id = $1`;
+  const query = `SELECT * FROM todos WHERE id = $1;`;
   const values = [id];
   const result = await client.query(query, values);
 
@@ -73,7 +73,22 @@ const getTodo = tryCatch(async (req, res) => {
   });
 });
 
-const deleteTodo = () => {};
+const deleteTodo = tryCatch(async (req, res) => {
+  const { id } = req.params;
+  const { success } = todoIdSchema.safeParse(+id);
+  if (!success) {
+    throw new CustomError("Id should be a number and is required", 403);
+  }
+  const query = `DELETE FROM TODOS WHERE id = $1 RETURNING *;`;
+  const values = [id];
+  const result = await client.query(query, values);
+
+  if (result.rows.length === 0) {
+    throw new CustomError("Todo with given id does not exist", 404);
+  }
+
+  res.sendStatus(204);
+});
 
 const updateTodo = () => {};
 
--- utils/customError.js
+++ utils/customError.js
@@ -1,5 +1,5 @@
 class CustomError extends Error {
-  constructor(message, statusCode) {
+  constructor(message, statusCode = 400) {
     super(message);
     this.statusCode = statusCode;
   }
